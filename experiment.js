3ccff074ec952531af6b6aa2a31ff61e7a3cf434
catou239 <catou239@hotmail.com>
2016-11-08 17:51:10 -0500

    finished a first percept-friendly version of the dm lab experiment

--- experiment.js
+++ experiment.js
@@ -4,15 +4,45 @@ function runExperiment(){
 	
 	serverPsych.request(function (settings){
 		//do things with the settings
+		var forcedChoiceStim;
+		var forcedChoiceTrialCounter = 0;		
+		var tobeadded;
 		
-		settings.timeline.forEach(function(element){
-			if(element.type == "rating"){
-				//im polishing up the rating block
+	    var build_node = {
+			type: "call-function",
+			func: function(){
+				choiceTimeline = ImageRater.getTimeline(20, [0], choiceTrialsNumber);
+				if(choiceTimeline === false){
+					jsPsych.endExperiment("Could not proceed with choice task");
+				}
 			}
-			else if(element.type =='forcedchoice'){
+	    };
+			    
+	    
+		settings.timeline.forEach(function(block, idx, timeline){
+			if(block.type == "rating"){
+				//im polishing up the rating block
+				var tobeadded= idx + 1;
+				var ratingStims = jsPsych.randomization.shuffle(settings.resources.images);				
+				block.timeline = {stimuli : ratingStims};							
+			}
+			else if(block.type =='forcedchoice'){
+				//im polishing up the forcedChoice block
+				block.on_finish = function(data){
+					forcedChoiceTrialCounter++;
+				}
 				
+				block.timeline = (function(){
+					for(var i=0; i < settings.length; i++){
+						var blockBuilder;
+						blockBuilder.push({stimuli: function(){return forcedChoiceStim[forcedChoiceTrialCounter]}});
+					};
+					return blockBuilder;
+				});
 			}
 		});
+		
+		settings.timeline.splice(tobeadded, 0, build_node);
 		
 		jsPsych.init({
 			timeline: settings.timeline,
